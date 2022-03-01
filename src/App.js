a554331ce38d061cc907e268f9a81e0658d91d7d
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2022-03-01 18:55:35 +0700

    complete sign in Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Login from './Login';
 import Register from './Register';
 
 function App() {
 
   return (
     <main className="App">
-      <Register />
+      <Login />
     </main>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,121 @@
+import { useContext, useRef, useState, useEffect } from "react";
+import AuthContext from "./context/AuthProvider";
+import axios from "./api/axios";
+
+const LOGIN_URL = "/auth";
+
+const Login = () => {
+  const { setAuth } = useContext(AuthContext);
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+
+  const onsubmitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ user, pwd }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            withCredential: true,
+          },
+        }
+      );
+      console.log(response?.data);
+      const accessToken = response?.data?.accessToken;
+      const roles = response?.data?.roles;
+
+      setAuth({ user, pwd, roles, accessToken });
+
+      setUser("");
+      setPwd("");
+      setSuccess(true);
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.response?.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
+      }
+      errRef.current.focus();
+    }
+  };
+
+  return (
+    <>
+      {success ? (
+        <section>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <a href="#"></a>Go to Home
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <h1>Sign In</h1>
+          <form onSubmit={onsubmitHandler}>
+            <label htmlFor="username">Username:</label>
+            <input
+              required
+              value={user}
+              type="text"
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => {
+                setUser(e.target.value);
+              }}
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+              required
+              value={pwd}
+              type="password"
+              id="password"
+              onChange={(e) => {
+                setPwd(e.target.value);
+              }}
+            />
+            <button>Sign In</button>
+          </form>
+          <p>
+            Need an Account? <br />
+            <span className="line">
+              {/* {put router link here} */}
+              <a href="">Sign Up</a>
+            </span>
+          </p>
+        </section>
+      )}
+    </>
+  );
+};
+
+export default Login;
